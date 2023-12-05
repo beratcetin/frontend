95c35ac6157e7c9a814a3757406b8ba23c8d6fd1
asimsinan <asimyuksel@sdu.edu.tr>
2023-12-05 12:34:48 +0300

    fix

--- src/services/http-common.jsx
+++ src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekanbul-frontend.vercel.app/venue.json",
+  baseURL: "https://mekanbul-frontend.vercel.app/venues.json",
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json;charset=UTF-8"
